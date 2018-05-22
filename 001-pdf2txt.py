db27d71e05d68ab75cff0e1ee5b83b5bd098577a
Hennadii Dobrovolskyi <hdobrovolskyi@envionsoftware.com>
2018-05-22 12:13:40 +0300

    + thd(r) baseline added

--- 001-pdf2txt.py
+++ 001-pdf2txt.py
@@ -8,20 +8,19 @@ import ate
 from os import listdir
 from os.path import isfile, join, isdir
 
-
-
 config = ConfigParser.ConfigParser()
 config.readfp(open('config.ini'))
 
 pdf_dir=config.get('main', 'pdf_dir')
 txt_dir=config.get('main', 'txt_dir')
 
-pdf_files=[ (pdf_dir, f) for f in listdir(pdf_dir) if isfile(join(pdf_dir, f)) and f.lower().endswith(".pdf")]
+pdf_files=sorted([ (pdf_dir, f) for f in listdir(pdf_dir) if isfile(join(pdf_dir, f)) and f.lower().endswith(".pdf")])
 
-# print pdf_files
+print pdf_files
 for f in pdf_files:
-    print pdf_dir,'/',f[1],"=>",txt_dir,'/',f[1]+'.txt'
-    ftxt = open(join(txt_dir, f[1]+'.txt'),'w')
+    fpath_out=join(txt_dir, f[1][:-4]+'.txt')
+    print pdf_dir,'/',f[1],"=>", fpath_out
+    ftxt = open(fpath_out,'w')
     ftxt.write(ate.pdf2text(join(f[0], f[1])).replace("\n"," "))
     ftxt.close()
-    #print 
+
